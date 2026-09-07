f24ee19a95943d2a2e55b7cffcb3ad0ad1d5cf05
Sakshi Miglani <miglanisakshi89@gmail.com>
2026-09-07 20:43:59 +0530

    Fix WhatsAppLog import path spelling

--- src/controllers/admincontroller.js
+++ src/controllers/admincontroller.js
@@ -22,7 +22,7 @@ const sendDualNotification = async ({ userId, fcmToken, socketEvent, title, body
         }
     }
 };
-const WhatsAppLog = require('../models/WhatsAppoLg');
+const WhatsAppLog = require('../models/WhatsAppLog');
 const { sendWhatsAppNotification } = require('../services/whatsappService');
 
 // Admin WhatsApp Send Controller
