d4a9281b5a6ced2e8b1bd16dd63cee5eb1d1551d
Sakshi Miglani <miglanisakshi89@gmail.com>
2026-09-08 13:00:42 +0530

    Fix Nodemailer IPv6 ENETUNREACH error by adding family 4

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -6,14 +6,16 @@ const dotenv = require('dotenv');
 const Withdrawal = require('../models/withdrawal'); // 👈 Ise sabse upar baki const ke sath jodein
 dotenv.config();
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    family: 4,
     auth: {
-        user: process.env.EMAIL_USER, 
-        pass: process.env.EMAIL_PASS  
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 });
 const otpDatabase = {};
-// otp..............................
 exports.sendOtpController = async (req, res) => {
     try {
         const { email } = req.body;
