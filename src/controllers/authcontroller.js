a0a61f6cc0e87082e0c7fcdaa55187df7c9adfec
Sakshi Miglani <miglanisakshi89@gmail.com>
2026-09-08 13:20:53 +0530

    Fix Gmail SMPT connection

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -7,12 +7,14 @@ const Withdrawal = require('../models/withdrawal'); // 👈 Ise sabse upar baki 
 dotenv.config();
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    family: 4,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        family: 4
     }
 });
 const otpDatabase = {};
