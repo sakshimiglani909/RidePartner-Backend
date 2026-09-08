9817c360832525b8db39b09ee77a07962090031c
Sakshi Miglani <miglanisakshi89@gmail.com>
2026-09-08 22:59:13 +0530

    Update Nodemailer SMTP config

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -7,9 +7,10 @@ const Withdrawal = require('../models/withdrawal');
 dotenv.config();
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true, 
     family: 4,
+    connectionTimeout: 10000,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
